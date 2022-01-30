6dff916278c66c7c576fd7a13648e198922357ef
yaswankar <yaswankar1998@gmail.com>
2022-01-30 22:12:55 +0530

    Changes: First Commit

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="main-layout">
+      <slot name="header">
+          <div class="menu-bar">
+              <button :class="{'hamb-container change': toggle, 'hamb-container': !toggle }" @click="toggleHamburger">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+              </button>
+          </div>
+      </slot>
+      <slot name="middlePane"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            toggle: false
+        };
+    },
+    methods: {
+        toggleHamburger() {
+            this.toggle = !this.toggle;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.main-layout {
+    .menu-bar {
+        display: flex;
+        position: relative;
+        align-items: center;
+        height: 56px;
+        background-color: #3f51b5;
+        border: 1px solid rgba(156,152,152,.5);
+        .bar1, .bar2, .bar3 {
+            width: 33px;
+            height: 4px;
+            background-color: #fff;
+            margin: 6px 0;
+            transition: 0.4s;
+        }
+        .hamb-container {
+            background: #3f51b5;
+            border: none;
+            outline: none;
+        }
+        .change {
+            .bar1 {
+                -webkit-transform: rotate(-45deg) translate(-7px, 6px);
+                transform: rotate(-45deg) translate(-7px, 6px);
+            }
+            .bar2 {opacity: 0;}
+            .bar3 {
+                -webkit-transform: rotate(45deg) translate(-8px, -8px);
+                transform: rotate(45deg) translate(-8px, -8px);
+            }
+        }
+    }
+}
+</style>
